d02a4fda0b716f9c7b877efdbf91d0418762f6f3
Alejandro Uribe <uribe.alejandro30@gmail.com>
2021-11-21 17:10:06 -0300

    Desarrollando desafio 3

---
+++ Desafio3.js
@@ -0,0 +1,26 @@
+const Contenedor = require('./Contenedor.js')
+const express = require('express')
+
+const app = express()
+
+const PORT = 8080
+
+const cc = new Contenedor('libros.txt')
+let productos = []
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`)
+    productos = cc.query()
+})
+
+app.get('/productos', (request, response) => {
+    
+    productos = cc.getAll()
+    response.send(productos)
+})
+
+app.get('/productoRandom', (request, response) => {
+    productos = cc.getAll()
+    const item = productos[Math.random()*productos.length | 0]
+    response.send(item)
+})
