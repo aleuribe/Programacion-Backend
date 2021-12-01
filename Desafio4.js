bdca724afbd08ffaf69bde0b08b333d90fbc84b2
Alejandro Uribe <uribe.alejandro30@gmail.com>
2021-11-30 23:15:20 -0300

    Desafio 4

---
+++ Desafio4.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const { addAbortSignal } = require('stream')
+const Contenedor = require('./libs/Contenedor.js')
+
+const {Router} = express
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+const router = Router()
+const libreria = new Contenedor(__dirname + '/data/productos.json')
+
+const PORT = 8080
+
+//Devuelve todos los productos: GET /api/productos
+router.get("/", (req, res) => {
+    return res.json(libreria.list)
+})
+
+//Devuelve un producto segun su ID: GET /api/productos/:id
+router.get("/:id", (req, res) => {
+    let id = req.params.id
+    return res.json(libreria.find(id))
+})
+
+//Recibe y agrega un producto y lo devuelve con su ID asignado: POST /api/productos
+router.post("/", (req, res) => {
+    let obj = req.body
+    return res.json(libreria.insert(obj))
+})
+
+//Recibe y actualiza un producto segun su id: PUT /api/productos/:id
+router.put("/:id", (req, res) => {
+    let obj = req.body
+    let id = req.params.id
+    return res.json(libreria.update(id,obj))
+})
+
+router.delete("/:id", (req,res) => {
+    let id = req.params.id
+    return(res.json(libreria.delete(id)))
+})
+
+app.use('/api/productos', router)
+app.use(express.static('./views'))
+
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`)
+})
+
+//Manejador de errores
+app.use(function(err,req,res,next){
+    console.log(err.stack)
+    res.status(500).send('Ocurrio un error: '+err)
+})
